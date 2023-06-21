fb450bbdf8486ef4c8b011bfe5faca95cfd2f72e
TEE2DWHY <isaacolorunfemi330@gmail.com>
2023-06-21 10:49:06 +0100

    Revert "added solidity coverage"
    This reverts commit 48c15d20cd173d721ae3afe88703de917a8d8ce7.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,11 @@
-require("dotenv").config();
-require("./tasks/blockNumber");
+require("dotenv").config()
+require("./tasks/blockNumber")
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const GEORLI = process.env.GOERLI_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_APIKEY = process.env.ETHERSCAN_APIKEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
@@ -25,12 +22,5 @@ module.exports = {
   solidity: "0.8.18",
    etherscan:{
     apiKey: ETHERSCAN_APIKEY
-  },
-  gasReporter:{
-    enabled: true,
-    // outputFile: "gas-report.txt",
-    currency: "USD",
-    coinmarketcap: COINMARKETCAP_API_KEY,
-    token: "MATIC"
   }
 };
